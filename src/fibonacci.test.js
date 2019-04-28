aedda10019427896dcf4f2dabe36356405434dc7
Karina Nowicka <knowicka@gmail.com>
2019-04-28 17:55:41 +0200

    test.each

--- src/fibonacci.test.js
+++ src/fibonacci.test.js
@@ -20,25 +20,33 @@ test('fib returns an array', () => {
 
 
 // 4) zwracane wartości
-test('num equal to 1 gives [0]', () => {
-    expect(fib(1)).toEqual([0]);
-});
-
-test('num equal to 2 gives [0, 1]', () => {
-    expect(fib(2)).toEqual([0, 1]);
-});
-
-test('num equal to 4 gives [0, 1, 1, 2]', () => {
-    expect(fib(4)).toEqual([0, 1, 1, 2]);
-});
-
-test('num equal to 10 gives [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]', () => {
-    expect(fib(10)).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+test.each`
+    num   | expected
+    ${1}  | ${[0]}
+    ${2}  | ${[0, 1]}
+    ${3}  | ${[0, 1, 1]}
+    ${4}  | ${[0, 1, 1, 2]}
+    ${10} | ${[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]}
+`('num equal to $num gives $expected', ({num, expected}) => {
+    expect(fib(num)).toEqual(expected);
 });
 
 test('args: 3, "str" and {} give [0, 1, 1]', () => {
     expect(fib(3, "str", {})).toEqual([0, 1, 1]);
 });
+
+
+// test.each`
+//     num   |   args   | expected
+//     ${1}  |  ${[]}   | ${[0]}
+//     ${2}  |  ${[]}   | ${[0, 1]}
+//     ${4}  |  ${[]}   | ${[0, 1, 1, 2]}
+//     ${10} |  ${[]}   | ${[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]}
+//     ${3}  | ${['str', {}]} | ${[0, 1, 1]}
+// `('params: $num and $args give(s) $expected', ({num, args, expected}) => {
+//     expect(fib(num, ...args)).toEqual(expected);
+// });
+
 
 
 // 5) rzucanie błędem
